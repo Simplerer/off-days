e6fb0f25c14f70b3bf94953164a604c5e610e004
A Stone <simplererstone@gmail.com>
2023-03-15 16:48:21 -0400

    basic setup for front

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,40 @@
 import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/NavBar';
+
+const httpLink = createHttpLink({ uri: '/graphql' });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
 
 function App() {
   return (
-    <h1>Hello y'all!</h1>
+
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Routes>
+
+        </Routes>
+      </Router>
+    </ApolloProvider>
+
   );
 }
 
